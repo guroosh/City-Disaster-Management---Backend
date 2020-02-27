d582b81f4b97fd8cd41cffab4311baf38f7c7599
DeepthiRajappan <deepthirajappan97@gmail.com>
2020-02-27 16:09:04 +0000

    Revert "Registration Module -> create doc test added"
    This reverts commit e86ef54556804a3fff606e936ce5e928c1ea7737

--- RSCD_tests/Registration/Registration_BLTests.cs
+++ RSCD_tests/Registration/Registration_BLTests.cs
@@ -1,24 +1,19 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Moq;
 using Registration.BusinessLogic;
 using Registration.DataAccess.Manager;
 using Registration.DataAccess.Repository;
 using Registration.DataEntry.DataAccess.Context;
 using Registration.Mqtt;
 using RSCD.Model.Configration;
-using RSCD.Model.Message;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
-using Xunit;
 
 namespace RSCD_tests.Registration
 {
-    public class Registration_BLTests
+    class Registration_BLTests
     {
-        private readonly ServiceProvider ServiceProvider;
-        private void SetupServices()
+     private void setupServices()
         {
             ServiceCollection services = new ServiceCollection();
             services.Configure<Mqtt_Settings>(options =>
@@ -39,28 +34,7 @@ namespace RSCD_tests.Registration
             services.AddScoped<IUsersCollection, Registration_CM>();
             services.AddMvc(option => option.EnableEndpointRouting = false);
         }
-        [Fact]
-        public async Task CreateAsyncRegistrationTest()
-        {
-            SetupServices();
-            var MockRequest = new Mock<NewUser>();
-            MockRequest.Object.ReferenceCode = "Android";
-            MockRequest.Object.EmailId = "palkarm@tcd.ie";
-            MockRequest.Object.Password = "1234";
-            MockRequest.Object.IsCommonUser = true;
-
-            bool response = true;
-            var businessLogic = ServiceProvider.GetRequiredService<Registration_BL>();
-            //Act
-            var result = await businessLogic.GetAllDocumentsAsync(MockRequest.Object);
-
-
-
-            //Assert
-            Assert.True((bool)result);
-        }
-
     }
-
+    
 
 }
